1fd2e25c27e8973f211eb0858004fcda0dff5e58
Mateusz Kozera <01169578@pw.edu.pl>
2022-04-11 18:35:49 +0200

    Plik ze strukturą grafową .h

---
+++ Utility.h
@@ -0,0 +1,27 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+#include <ctype.h>
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+struct AdjListNode
+{
+    int dest;
+    int weight;
+    struct AdjListNode* next;
+};
+struct AdjList
+{
+
+    // Pointer to head node of list
+    struct AdjListNode* head;
+};
+struct Graph
+{
+    int V;
+    struct AdjList* array;
+};
+
+
+void otworzenie_pliku_i_sprawdzenie_danych(char * plikwej, graf_str ** graf);	
+#endif
